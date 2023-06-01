96b96d27316b3ae60c8a8fbc9cd4889e932170f7
Roman <kavoriya@gmail.com>
2023-06-01 18:33:51 +0500

    Add input field
    Add input field, where user can input a city. Show the weather
    in console.

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
+let inputButton = document.getElementById('inputButton');
+let inputField = document.getElementById('inputField');
+
+inputButton.addEventListener('click', () => {
+   if (inputField.value != '') {
+      showData(inputField.value);
+   }
+})
+
 async function getData(location) {
    let url = 'http://api.weatherapi.com/v1/current.json?key=cb4c3b036354487e82a82318231405&q=';
-   try {
-      const response = await fetch(url + location, { mode: 'cors' });
-      return await response.json();
-   } catch (err) {
-      console.log(err);
-   }
+   const response = await fetch(url + location, { mode: 'cors' });
+   return response.json();
 }
 
 function processData(response) {
-   let weatherObject = {
+   return {
+      location: response.location.name,
       temp: response.current.temp_c,
       condition: response.current.condition.text,
       cloud: response.current.cloud,
       wind: response.current.wind_kph,
       humidity: response.current.humidity,
    }
-   return weatherObject;
 }
-let data = getData('Tyumen');
-data.then(function(data) {
-   let newData = processData(data);
-   console.log(newData);
-})
+
+async function showData(location) {
+   try {
+      let data = await getData(location);
+      let processedData = processData(data);
+      console.log(processedData);
+   } catch (err) {
+      console.log(err);
+   }
+}
