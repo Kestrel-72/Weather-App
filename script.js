89cd709744ee5d37814d9ed1f43abe84f2a32037
Roman <kavoriya@gmail.com>
2023-06-03 13:30:17 +0500

    Display weather forecast on basic UI

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 let inputButton = document.getElementById('inputButton');
 let inputField = document.getElementById('inputField');
 
+let tempP = document.querySelector('.temp-p');
+let conditionP = document.querySelector('.condition-p');
+let cloudP = document.querySelector('.cloud-p');
+let windP = document.querySelector('.wind-p');
+let humidityP = document.querySelector('.humidity-p');
+
 inputButton.addEventListener('click', () => {
    if (inputField.value != '') {
-      showData(inputField.value);
+      showWeatherData(inputField.value);
+   } else {
+      showWeatherData('oslo');
    }
 })
 
-async function getData(location) {
+async function getWeatherData(location) {
    let url = 'http://api.weatherapi.com/v1/current.json?key=cb4c3b036354487e82a82318231405&q=';
    const response = await fetch(url + location, { mode: 'cors' });
    return response.json();
 }
 
-function processData(response) {
+function processWeatherData(response) {
    return {
       location: response.location.name,
       temp: response.current.temp_c,
@@ -24,11 +32,20 @@ function processData(response) {
    }
 }
 
-async function showData(location) {
+function populateWeatherInfo(data) {
+   tempP.textContent = `Temperature ${data.temp} C°`;
+   conditionP.textContent = data.condition;
+   cloudP.textContent = `Cloudiness ${data.cloud}`;
+   windP.textContent = `Wind ${data.wind} km/h`;
+   humidityP.textContent = `Humidity ${data.humidity}`;
+}
+
+async function showWeatherData(location) {
    try {
-      let data = await getData(location);
-      let processedData = processData(data);
+      let data = await getWeatherData(location);
+      let processedData = processWeatherData(data);
       console.log(processedData);
+      populateWeatherInfo(processedData);
    } catch (err) {
       console.log(err);
    }
