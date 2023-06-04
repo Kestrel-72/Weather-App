5d76d0cf607a2ee98638d4cc671f2a157a1478f6
Roman <kavoriya@gmail.com>
2023-06-04 15:33:36 +0500

    Add style

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let inputButton = document.getElementById('inputButton');
 let inputField = document.getElementById('inputField');
 
+let locationP = document.querySelector('.location');
 let tempP = document.querySelector('.temp-p');
 let conditionP = document.querySelector('.condition-p');
 let cloudP = document.querySelector('.cloud-p');
 let windP = document.querySelector('.wind-p');
 let humidityP = document.querySelector('.humidity-p');
+let uvP = document.querySelector('.uv-p');
 
 inputButton.addEventListener('click', () => {
    if (inputField.value != '') {
@@ -37,11 +39,13 @@ function processWeatherData(response) {
 }
 
 function populateWeatherInfo(data) {
+   locationP.textContent = `${data.location}, ${data.country}`,
    tempP.textContent = `Temperature ${data.temp} C°`;
    conditionP.textContent = data.condition;
-   cloudP.textContent = `Cloudiness ${data.cloud}`;
+   cloudP.textContent = `Cloudiness ${data.cloud}%`;
    windP.textContent = `Wind ${data.wind_kph} km/h`;
-   humidityP.textContent = `Humidity ${data.humidity}`;
+   humidityP.textContent = `Humidity ${data.humidity}%`;
+   uvP.textContent = `${data.uv}`;
 }
 
 async function showWeatherData(location) {
